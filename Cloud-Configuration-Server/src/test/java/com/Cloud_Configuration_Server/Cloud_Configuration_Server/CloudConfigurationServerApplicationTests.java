acca9596ef8775f625867c87ff1275e50d21169b
nourfmohammed1 <nourfmohammed1@gmail.com>
2026-01-14 15:22:52 +0200

    add cloud configuration server

---
+++ Cloud-Configuration-Server/src/test/java/com/Cloud_Configuration_Server/Cloud_Configuration_Server/CloudConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cloud_Configuration_Server.Cloud_Configuration_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
